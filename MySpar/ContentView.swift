6f17997c8949ef23213d76bb138295b7b0b8e3f0
Liliya Andreeva <Liliya_yanbaeva@mail.ru>
2024-08-05 16:05:47 +0500

    Initial Commit

---
+++ MySpar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySpar
+//
+//  Created by Лилия Андреева on 05.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Лилия Андреева on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
